5c0ef0ed2ade229e5733fd4e4db37d680a989926
Laxmi530 <76505459+Laxmi530@users.noreply.github.com>
2025-02-27 15:22:52 +0530

    Add files via upload

--- Config_Data.py
+++ Config_Data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def load_config_details(file):
     try:
-        with open(file, 'r') as read_data:
+        with open(file, 'r', encoding = "utf-8") as read_data:
             config_data = yaml.safe_load(read_data)
             return config_data
     except yaml.YAMLError as exc:
@@ -12,8 +12,10 @@ def load_config_details(file):
 
 def correct_path():
     possible_paths = [Path(os.path.join(os.getcwd(), "config.yml")), Path(os.path.join(os.getcwd(), "config", "config.yml"))]  
-    for dir in possible_paths:
-        if os.path.exists(dir):
-            return dir
+    for paath in possible_paths:
+        if os.path.exists(paath):
+            return paath
+    return None
+
 
 load_config_data = load_config_details(correct_path())
